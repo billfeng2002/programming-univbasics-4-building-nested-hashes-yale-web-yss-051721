65e8fe4320af58edaae9d2e7eef75063e66c7da7
Bill Feng <bill.feng@yale.edu>
2021-05-23 06:50:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,6 +13,7 @@ def monopoly_with_second_tier
   basehash=base_hash
   basehash[:railroads][:pieces]=4
   basehash[:railroads][:rent_in_dollars]={}
+  basehash[:railroads][:names]={}
   basehash
 end
 
