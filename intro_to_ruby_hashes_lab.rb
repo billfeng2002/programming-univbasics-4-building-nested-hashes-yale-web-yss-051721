5e473ad893a96b60b05edc18175a9a81ce947596
Bill Feng <bill.feng@yale.edu>
2021-05-23 06:54:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,6 +29,8 @@ def monopoly_with_third_tier
         secondtierhash[:railroads][:rent_in_dollars][:four_pieces_owned]=200;
   secondtierhash[:railroads][:names][:reading_railroad]={}
     secondtierhash[:railroads][:names][:pennsylvania_railroad]={}
+      secondtierhash[:railroads][:names][:b_and_o_railroad]={}
+
 
 
   
