77ba21c57ec7740816e1ee0badaed45f99a35302
Bill Feng <bill.feng@yale.edu>
2021-05-23 06:51:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,10 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  secondtierhash=monopoly_with_second_tier
+  secondtierhash[:railroads][:rent_in_dollars][:one_piece_owned]=25
+  
+  secondtierhash
 end
 
 def monopoly_with_fourth_tier
